a07161e59ba72445268e1e424d90cb8fdd67b89b
stephan kreft <stephan.kreft>
2015-11-01 17:16:55 +0100

    added background image

--- pointerapp/ViewController.swift
+++ pointerapp/ViewController.swift
@@ -34,28 +34,28 @@ class ViewController: UIViewController {
         
         btnAbout.layer.cornerRadius = 10
         btnAbout.clipsToBounds = true
-        btnAbout.layer.borderColor = UIColor.whiteColor().CGColor
+        btnAbout.layer.borderColor = UIColor.blackColor().CGColor
         btnAbout.layer.borderWidth = 2
         
         btnOpeningHours.layer.cornerRadius = 10
         btnOpeningHours.clipsToBounds = true
-        btnOpeningHours.layer.borderColor = UIColor.whiteColor().CGColor
+        btnOpeningHours.layer.borderColor = UIColor.blackColor().CGColor
         btnOpeningHours.layer.borderWidth = 2
         
         
         btnFindUs.layer.cornerRadius = 10
         btnFindUs.clipsToBounds = true
-        btnFindUs.layer.borderColor = UIColor.whiteColor().CGColor
+        btnFindUs.layer.borderColor = UIColor.blackColor().CGColor
         btnFindUs.layer.borderWidth = 2
         
         btnAppointment.layer.cornerRadius = 10
         btnAppointment.clipsToBounds = true
-        btnAppointment.layer.borderColor = UIColor.whiteColor().CGColor
+        btnAppointment.layer.borderColor = UIColor.blackColor().CGColor
         btnAppointment.layer.borderWidth = 2
         
         btnServices.layer.cornerRadius = 10
         btnServices.clipsToBounds = true
-        btnServices.layer.borderColor = UIColor.whiteColor().CGColor
+        btnServices.layer.borderColor = UIColor.blackColor().CGColor
         btnServices.layer.borderWidth = 2
         
         
